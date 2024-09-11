5a4241f3d8313fcff72d707939858b5d57c0f395
FaisalAlharbidev <164782595+FaisalAlharbidev@users.noreply.github.com>
2024-09-11 12:57:24 +0300

    Add files via upload

---
+++ domLab-1.js
@@ -0,0 +1,13 @@
+
+//Q1______________
+let h1_tag=document.getElementsByClassName("name")[0].style.backgroundColor="blueviolet"
+
+h1_tag=document.getElementsByClassName("name")[0].style.color="white"
+
+
+//Q2______________
+
+let decrption =document.getElementById("decrp").style.fontWeight="bold"
+
+let decrpClass = document.element.classList.add("decrpClass");
+
---
+++ index.js
@@ -0,0 +1,9 @@
+let ss = document.getElementById("body").style.color="red";
+ ss = document.getElementById("body").style.backgroundColor="black";
+
+
+ function changeText(id) {
+  id.innerHTML = "Ooops!";
+}
+
+// ss.innerHTML("f")
